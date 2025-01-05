5d57b4ae8a108267fa2e6634edefdae804290f23
Lucas Bonanni <bonanni.lucas.esteban@gmail.com>
2025-01-05 18:42:00 -0300

    last changes

--- doc_downloader.py
+++ doc_downloader.py
@@ -15,3 +15,9 @@ folder = './api-def'
 #     with open(os.path.join(folder, file_name), 'wb') as fd:
 #         fd.write(res.content)
 
+from billing_charge_api_service_client.api.compensation import get_v_1_compensation_compensation_id_json_to_sap
+from billing_charge_api_service_client.client import AuthenticatedClient
+
+auth = AuthenticatedClient('','')
+
+get_v_1_compensation_compensation_id_json_to_sap.sync_detailed(compensation_id=12345,client=auth)
